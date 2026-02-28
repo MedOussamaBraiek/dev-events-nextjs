270f50ca0db61cfe1c0ea9e931a55629a45a4e54
MedOussamaBraiek <mohamed.braiek@esprit.tn>
2026-02-28 01:34:10 +0100

    Booking form

--- components/BookEvent.tsx
+++ components/BookEvent.tsx
@@ -1,5 +1,30 @@
+"use client";
+
+import { useState } from "react";
+
 const BookEvent = () => {
-  return <div>BookEvent</div>;
+  const [email, setEmail] = useState("");
+  const [isSubmitted, setIsSubmitted] = useState(false);
+
+  return (
+    <div className="book-event">
+      {isSubmitted ? (
+        <p className="text-sm">Thank you for signing up!</p>
+      ) : (
+        <form>
+          <div>
+            <label htmlFor="email">Email address</label>
+            <input
+              type="email"
+              id="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+        </form>
+      )}
+    </div>
+  );
 };
 
 export default BookEvent;
