bc16fb9e31645b546a58b29f04f9ceccc8e48a08
MedOussamaBraiek <mohamed.braiek@esprit.tn>
2026-03-01 23:11:41 +0100

    update

--- components/BookEvent.tsx
+++ components/BookEvent.tsx
@@ -6,12 +6,20 @@ const BookEvent = () => {
   const [email, setEmail] = useState("");
   const [isSubmitted, setIsSubmitted] = useState(false);
 
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    setTimeout(() => {
+      setIsSubmitted(true);
+    }, 1000);
+  };
+
   return (
-    <div className="book-event">
+    <div id="book-event">
       {isSubmitted ? (
         <p className="text-sm">Thank you for signing up!</p>
       ) : (
-        <form>
+        <form onSubmit={handleSubmit}>
           <div>
             <label htmlFor="email">Email address</label>
             <input
@@ -19,8 +27,13 @@ const BookEvent = () => {
               id="email"
               value={email}
               onChange={(e) => setEmail(e.target.value)}
+              placeholder="Enter your email adress"
             />
           </div>
+
+          <button type="submit" className="button-submit">
+            Submit
+          </button>
         </form>
       )}
     </div>
